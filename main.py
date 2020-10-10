dfb0035ff05715d6ffa24a488e235cb15f5ebea7
tjoudrey <tjoudrey@clack-machine>
2020-10-10 16:17:32 -0300

    comments removed

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# requires ffmpg
-
 import click
 import http.server
 import socketserver
@@ -19,18 +17,9 @@ def stream(port, mp4):
     serveStream(port, mp4)
 
 
-# @cli.command(help="Generate and Stream a video")
-# @click.option('--port', default=8000, help="Specify the port to serve the stream over")
-# def streamGeneratedVideo(port):
-#     generateMp4()
-#     generateHlsPlaylist(mp4)
-#     serveStream(port)
-
-
 def generateHlsPlaylist(videoFile):
-    os.system('cd hls_files/')
-    os.system('rm -rfy *')
-    os.system('cd ..')
+    os.system('rm -rf hls_files')
+    os.system('mkdir hls_files/')
     os.system(
         'ffmpeg -i '+videoFile+' -vn hls_files/'+videoFile.split('.')[0]+'.mp3 -c:v libx264 -c:a aac -strict -2 -f hls -hls_list_size 0 hls_files/'+videoFile.split('.')[0]+'.m3u8')
 
@@ -39,7 +28,6 @@ def serveStream(port, videoFile):
     os.chdir('hls_files')
     Handler = http.server.SimpleHTTPRequestHandler
     httpd = socketserver.TCPServer(("", port), Handler)
-    print("serving at port", port)
     print("Stream available at http://localhost:" +
           str(port)+"/"+videoFile.split('.')[0]+".m3u8")
     httpd.serve_forever()
